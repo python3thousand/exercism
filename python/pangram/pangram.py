f5de2f9575229ec385842acc3cebfa71f04a3088
jhanna9 <hannajm9@gmail.com>
2018-11-05 12:46:52 -0800

    worked on pangram exercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,35 @@
+import string
+
+
 def is_pangram(sentence):
-    pass
+    alphabet = string.ascii_lowercase
+    in_sentence = []
+
+    if sentence:
+        if sentence == alphabet:
+
+            return True
+
+        else:
+            for l in sentence.lower():
+                if l not in alphabet:
+                    continue
+                elif len(in_sentence) >= 1 and l in in_sentence:
+                    continue
+                elif l in alphabet and l not in in_sentence:
+                    in_sentence.append(l)
+
+                    if len(in_sentence) == 26:
+
+                        return True
+
+                    else:
+                        continue
+
+                else:
+
+                    return False
+
+    else:
+
+        return False
